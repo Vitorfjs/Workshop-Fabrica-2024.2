c672167b0d40eb0752858e7a5ee6236903ec04bb
Vitor <vitor.acad@hotmail.com>
2024-08-24 00:49:23 -0300

    Adiciona e refatora testes, limpa e organiza código
    - Adiciona teste para a listagem de personagens, verificando status code 200 OK.
    - Refatora viewsets e serializers para melhorar legibilidade e manutenção.
    - Limpa código removendo imports não utilizados e ajustando formatação.
    - Atualiza testes para endpoints de autenticação JWT e inclui casos de teste adicionais.

--- myapp/viewsets.py
+++ myapp/viewsets.py
@@ -1,11 +1,14 @@
 import requests
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.decorators import action
 from .models import Character
 from .serializers import CharacterSerializer
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+
+class CharacterPagination(PageNumberPagination):
+    page_size = 10
 
 class CharacterFilter(filters.FilterSet):
     name = filters.CharFilter(lookup_expr='icontains')
@@ -19,6 +22,7 @@ class CharacterViewSet(viewsets.ModelViewSet):
     serializer_class = CharacterSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = CharacterFilter
+    pagination_class = CharacterPagination
 
     def list(self, request):
         # Sincronizar dados da API externa com o banco de dados
@@ -44,18 +48,3 @@ class CharacterViewSet(viewsets.ModelViewSet):
         
         serializer = CharacterSerializer(characters, many=True)
         return Response(serializer.data)
-
-    @action(detail=True, methods=['post'])
-    def create_character(self, request):
-        # Criação de personagem não é necessário aqui, ModelViewSet já lida com isso
-        return Response({'detail': 'Não permitido'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    @action(detail=True, methods=['put', 'patch'])
-    def update_character(self, request, pk=None):
-        # Atualização de personagem não é necessário aqui, ModelViewSet já lida com isso
-        return Response({'detail': 'Não permitido'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    @action(detail=True, methods=['delete'])
-    def delete_character(self, request, pk=None):
-        # Exclusão de personagem não é necessário aqui, ModelViewSet já lida com isso
-        return Response({'detail': 'Não permitido'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
